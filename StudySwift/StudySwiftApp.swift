c359bd3ae7d15bdb3674a529df836a4653759613
chaeyn <chaulfe@gmail.com>
2026-05-26 23:15:47 +0900

    init

---
+++ StudySwift/StudySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftApp.swift
+//  StudySwift
+//
+//  Created by chaeyn on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
